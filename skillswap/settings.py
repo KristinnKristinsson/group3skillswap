84d7ed283ad1d25152c30d62bf912b76e425ff11
Mishkuat <mishkuatsabir11@gmail.com>
2025-07-14 16:00:10 +0200

    added logout functionality for the users.

--- skillswap/settings.py
+++ skillswap/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIR = [STATIC_DIR]
+STATICFILES_DIRS = [STATIC_DIR]
 
 #MEDIA
 MEDIA_ROOT = MEDIA_DIR
--- user_authentication/views.py
+++ user_authentication/views.py
@@ -8,7 +8,7 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def index(request):
-    return render(request,'skills/index.html')
+    return render(request,'index.html')
 
 
 @login_required 
